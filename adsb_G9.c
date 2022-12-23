1e20c89400943313fb5ee1846bffd29d7fa4068e
21km4 <kouki.mizumoto@fuji.waseda.jp>
2022-12-24 00:18:33 +0900

    edit distance

--- adsb_G9.c
+++ adsb_G9.c
@@ -1,22 +1,85 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <math.h>
 
 #include "ask.h"
 
-int DecideTrials(const int p_ins, const int p_sub, const int p_del) {
-	return 5;
-}
-
-void RestoreSignal(char** qs, char* r_signal, const int q_trials) {
-	for (int i = 0; i < 200; i++){
-		if (qs[0][i] != '\0') {
-			r_signal[i] = '\0';
-			break;
-		}
-		for (int j = 0; j < q_trials; j++) {
-
+void min_object(int *array, int num, int *min_index)
+{
+	int min = INT_MAX;
+	*min_index = -1;
+	for (int i = 0; i < num; i++)
+	{
+		if (array[i] < min)
+		{
+			min = array[i];
+			*min_index = i;
 		}
 	}
+}
+
+// 最小の数を探す
+int min(int a, int b, int c)
+{
+	return a > b ? (b > c ? c : b) : (a > c ? c : a);
+}
+
+// レーベンシュタイン距離を求める
+int LevenshteinDistance(char *str1, char *str2)
+{
+	int lenstr1 = strlen(str1) + 1;
+	int lenstr2 = strlen(str2) + 1;
+	int d[lenstr1][lenstr2];
+	int i1 = 0, i2 = 0, cost = 0;
+
+	for (; i1 < lenstr1; i1++)
+		d[i1][0] = i1;
+	for (; i2 < lenstr2; i2++)
+		d[0][i2] = i2;
+
+	for (i1 = 1; i1 < lenstr1; i1++)
+	{
+		for (i2 = 1; i2 < lenstr2; i2++)
+		{
+			cost = str1[i1 - 1] == str2[i2 - 1] ? 0 : 1;
+			d[i1][i2] = min(d[i1 - 1][i2] + 1, d[i1][i2 - 1] + 1, d[i1 - 1][i2 - 1] + cost);
+		}
+	}
+
+	return d[lenstr1 - 1][lenstr2 - 1];
+}
+
+int PredictAnswer(char **S, char *q, int id, int *ans_dis, const int p_ins, const int p_sub, const int p_del)
+{
+	int length = strlen(q);
+	int ans_id = -1;
+	const double entropy = (p_ins + p_sub + p_del) / 15.0;
+	for (int id = 0; id < N; id++)
+	{
+		int min_dis = INT_MAX;
+		for (int i = 0; i < DATA_LENGTH; i += length / (5.0 - entropy))
+		{
+			char *temp = (char *)malloc(sizeof(char) * (length + 1));
+			strncpy(temp, S[id] + i, length);
+			temp[length] = '\0';
+			int dis = LevenshteinDistance(temp, q);
+			if (dis < min_dis)
+			{
+				min_dis = dis;
+				ans_id = id;
+			}
+			if (dis < length / 3) // 編集距離はQueryの長さにも依存するのでそれも考慮する
+			{
+				// 多分これであっているだろうから早とちりしてみる
+				// printf("abort\n");
+				return ans_id + 1;
+			}
+		}
+		ans_dis[id] += min_dis;
+	}
+	// printf("dis=%d\n", ans_dis);
+	return 0;
 }
 
 int main(int argc, char *argv[])
@@ -46,29 +109,29 @@ int main(int argc, char *argv[])
 	}
 #pragma endregion
 
-	const int q_trials = DecideTrials(p_ins, p_sub, p_del);
-
 	// p_ins, p_sub, p_del
 	for (int i = 0; i < Q; i++)
 	{
-		char **qs = (char **)malloc(sizeof(char *) * q_trials);
-		qs[0] = (char *)malloc(sizeof(char) * 200);
-		fscanf(input_file, "%s", qs[0]);
-		for (int j = 1; j < q_trials; j++)
-		{
-			qs[j] = (char *)malloc(sizeof(char) * 200);
-			qs[j] = ask(i + 1, argv[3]); // Query
-		}
-		
-		char* r_signal = (char* )malloc(sizeof(char) * 200);
-		RestoreSignal(qs, r_signal, q_trials);
+		int ans = 0;
+		int ans_dis[Q + 1] = {0};
+		char *q = (char *)malloc(sizeof(char) * 200);
+		fscanf(input_file, "%s", q);
 
-		int ans = 0; // diff to S[i]
+		// if (j != 0) q = ask(i + 1, argv[3]);
+		int t = PredictAnswer(S, q, i, ans_dis, p_ins, p_sub, p_del);
+		free(q); // 予測したらもういらないよね
+		if (t != 0)
+		{
+			ans = t;
+		}
+		else
+		{
+			int min_index;
+			min_object(ans_dis, Q, &min_index);
+			ans = min_index + 1;
+		}
 
 		fprintf(output_file, "%d\n", ans);
-		for (int i = 0; i < q_trials; i++)
-			free(qs[i]);
-		free(qs);
 	}
 
 #pragma region FINALIZE
--- evaluate.c
+++ evaluate.c
@@ -7,14 +7,16 @@ int main(int argc, char* argv[]) {
     int p_ins, p_sub, p_del; // Insert, Substitute, Delete
     fscanf(answer_file, "%d %d %d", &p_ins, &p_sub, &p_del);
 
-    for (int i = 0; i < 100; i++) {
+    int correct = 0, i;
+    for (i = 0; i < 100; i++) {
         int input[100], answer[100];
-        char data[200];
-        fscanf(input_file, "%d", &input[i]);
+        char data[100];
+        if (fscanf(input_file, "%d", &input[i]) == EOF) break;
         fscanf(answer_file, "%d %s", &answer[i], data);
-
+        if (input[i] == answer[i]) correct++;
         printf("%d %d\t%s\n", input[i], answer[i], input[i] == answer[i] ? "OK" : "NG");
     }
+    printf("\n%d/%d Correct.\n", correct, i);
 
     fclose(input_file);
 	fclose(answer_file);
